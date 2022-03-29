24e65d73cfce84c679a72728465e08e4973d9ca5
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-03-29 15:47:21 -0600

    Tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#hello'
+  get 'posts/index'
+  get 'posts/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
